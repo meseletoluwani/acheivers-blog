12a6369735c0d7686d37e303867e9e3d6f5d5252
Goodnews Adewole <goodnews.adewole@example.com>
2023-05-01 18:01:34 +0100

    done

--- website/views.py
+++ website/views.py
@@ -1,9 +1,13 @@
 # This file will contain all of the routes related to the core blog application like the homepage etc
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint("views", __name__)
 
 @views.route("/")
 def home():
     return "Hello"
+
+@views.route("/signup")
+def signup():
+    return render_template("signup.html")
