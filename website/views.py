3e4454abcff4ab94dc83b3d956aea942aa88044d
Ayomikun Olalekan <ayomikunolalekan6@gmail.com>
2023-06-11 13:37:44 +0100

    Update post feature

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 # This file will contain all of the routes related to the core blog application like the homepage etc
 
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, abort
 from flask_login import login_required, current_user
 from .models import Post, User
 from . import db
@@ -52,8 +52,27 @@ def create_post():
 def view_post(post_title):
     post = Post.query.filter(Post.title == post_title).first()
     return render_template("view_post.html", user=current_user, post=post)
+
+
 @views.route("/posts/<username>", methods = ["GET", "POST"])
 @login_required
 def view__user_posts(username):
     post = Post.query.join(User).filter(User.username == username).all()
     return render_template("view_user_posts.html", user=current_user, posts=post)
+
+@views.route("/post/<post_title>/update", methods = ["GET", "POST"])
+@login_required
+def update_post(post_title):
+    post = Post.query.filter(Post.title == post_title).first()
+    # if post.author != current_user:
+    #     abort(403)
+    title = request.form.get("title")
+    image = request.form.get("image")
+    content = request.form.get("content")
+    if request.method == "POST":
+        post.title = title
+        post.image = image
+        post.content = content
+        db.session.commit()
+        flash("Post updated successfully", category="success")
+    return render_template("update_post.html", user=current_user,)
