237a07f18d617ba02108cdf8a24971c514dd7122
Goodnews Adewole <goodnews.adewole@example.com>
2023-06-13 11:55:56 +0100

    pushed

--- app.py
+++ app.py
@@ -2,12 +2,8 @@
 
 from website import create_app
 # app = create_app()
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app = create_app()
+#     app.run(debug=True)
 
-<<<<<<< HEAD
-
-=======
 app = create_app()
->>>>>>> 0e06106f55e7185599e0dd208965af1d4b9bb91c
